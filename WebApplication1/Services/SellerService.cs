6805f8b23cb0ae19b197b01760850e6bd67b04a7
Andre Pires <pirao@uol.com.br>
2021-08-10 09:47:00 -0300

    Foreign key not null

--- WebApplication1/Services/SellerService.cs
+++ WebApplication1/Services/SellerService.cs
@@ -19,6 +19,7 @@ namespace WebApplication1.Services {
         }
 
         public void Insert(Seller obj) {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
